469cb5d3c44781dbc02a3d61ca825d6cfe5a0af4
Zhihao Cao <1564758299@qq.com>
2025-01-02 14:52:26 +0100

    Update AutoEncode
    Update AutoEncode

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ project_root/
 │   ├── rawData.npy
 │   └── ...
 └── ...
+
+# Env
+
+conda env create -f eth_dl.yml
+conda activate eth_dl
+
+pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.0.0+cu117.html
--- models/autoencoder.py
+++ models/autoencoder.py
@@ -2,12 +2,14 @@
 import sys
 import os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch_cluster
 import torch_scatter
 from models.mgn import MeshGraphNet
+from utils.dataset import EncoderDecoderDataset
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
@@ -46,8 +48,8 @@ class MeshReduce(nn.Module):
             latent_size=internal_width,
             num_layers=num_layers,
             n_nodefeatures=output_node_features_dim,
-            n_edgefeatures_mesh=input_edge_features_dim,
-            n_edgefeatures_world=input_edge_features_dim,
+            n_edgefeatures_mesh=internal_width,
+            n_edgefeatures_world=internal_width,
             message_passing_steps=message_passing_steps//2
         )
 
@@ -84,16 +86,16 @@ class MeshReduce(nn.Module):
 
         return y.float(), x_idx, y_idx, weights
 
-    def encode(self, sample, position_mesh, position_pivotal):
+    def encode(self, sample, position_mesh, position_pivotal, batch_size):
         sample = self.encoder_processor(sample)
         node_features = self.PivotalNorm(sample['fluid'].node_attr)
         
-        nodes_index = torch.arange(node_features.shape[0]).to(node_features.device)
-        batch_mesh = nodes_index.repeat_interleave(node_features.shape[1])
-        position_mesh_batch = position_mesh.repeat(node_features.shape[0], 1)
-        position_pivotal_batch = position_pivotal.repeat(node_features.shape[0], 1)
+        nodes_index = torch.arange(batch_size).to(node_features.device)
+        batch_mesh = nodes_index.repeat_interleave(node_features.shape[0])
+        position_mesh_batch = position_mesh.repeat(batch_size, 1)
+        position_pivotal_batch = position_pivotal.repeat(batch_size, 1)
         batch_pivotal = nodes_index.repeat_interleave(
-            torch.tensor([len(position_pivotal)] * node_features.shape[0]).to(node_features.device)
+            torch.tensor([len(position_pivotal)] * batch_size).to(node_features.device)
         )
 
         node_features, _, _, _ = self.knn_interpolate(
@@ -104,6 +106,16 @@ class MeshReduce(nn.Module):
                                         batch_y=batch_pivotal,
         )
         
+        # print(node_features.shape)
+        
+        # print(position_mesh_batch.shape)
+        
+        # print(position_pivotal_batch.shape)
+        
+        # print(batch_mesh.shape)
+        
+        # print(batch_pivotal.shape)
+        
         sample['fluid'].node_attr = node_features
 
         #TODO: add the spatial information embedding to the pivotal nodes.
@@ -113,16 +125,27 @@ class MeshReduce(nn.Module):
         return sample
     
 
-    def decode(self, sample, position_mesh, position_pivotal):
+    def decode(self, sample, position_mesh, position_pivotal, batch_size):
         node_features = sample['fluid'].node_attr
 
-        nodes_index = torch.arange(node_features.shape[0]).to(x.device)
-        batch_mesh = nodes_index.repeat_interleave(node_features.shape[1])
-        position_mesh_batch = position_mesh.repeat(node_features.shape[0], 1)
-        position_pivotal_batch = position_pivotal.repeat(node_features.shape[0], 1)
+        nodes_index = torch.arange(batch_size).to(node_features.device)
+        # Fix the bug: node_features -> position_mesh
+        batch_mesh = nodes_index.repeat_interleave(position_mesh.shape[0])
+        position_mesh_batch = position_mesh.repeat(batch_size, 1)
+        position_pivotal_batch = position_pivotal.repeat(batch_size, 1)
         batch_pivotal = nodes_index.repeat_interleave(
-            torch.tensor([len(position_pivotal)] * node_features.shape[0]).to(x.device)
+            torch.tensor([len(position_pivotal)] * batch_size).to(node_features.device)
         )
+        
+        print(node_features.shape)
+        
+        # print(position_mesh_batch.shape)
+        
+        # print(position_pivotal_batch.shape)
+        
+        # print(batch_mesh.shape)
+        
+        # print(batch_pivotal.shape)
 
         node_features, _, _, _ = self.knn_interpolate(
                                         x=node_features,
@@ -132,21 +155,55 @@ class MeshReduce(nn.Module):
                                         batch_y=batch_mesh,
         )
         sample['fluid'].node_attr = node_features
+        
+        print(node_features.shape)
 
         sample = self.decoder_processor(sample)
         
         return sample
     
-    # def forward(self, sample) -> torch.Tensor:
-    #     """Encodes and processes a multigraph, and returns node features."""
-    #     sample = self.encode(sample)
+    def forward(self, sample, position_mesh, position_pivotal, batch_size):
+        """Encodes and processes a multigraph, and returns node features."""
+        sample = self.encode(sample, position_mesh, position_pivotal, batch_size)
 
-    #     return self.decode(sample)
-
-
-
+        return self.decode(sample, position_mesh, position_pivotal, batch_size)
 
 
 #%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+import numpy as np
+from utils import constant
+
+C = constant.Constant()
+
 if __name__ == '__main__':
-    pass
+    # Fix the bug: output_node_features_dim: 3 -> 64
+    output_node_features_dim: int = 64
+    internal_width: int = 64
+    num_layers: int = 2
+    input_node_features_dim: int = 3
+    input_edge_features_dim: int = 3
+    message_passing_steps: int = 15
+    
+    dataset = EncoderDecoderDataset()
+    sample = dataset[0]
+    
+    print(sample)
+
+    enc_doc_model = MeshReduce(input_node_features_dim,
+                                input_edge_features_dim,
+                                output_node_features_dim,
+                                internal_width,
+                                message_passing_steps,
+                                num_layers).to('cuda')
+    
+    position_mesh = torch.from_numpy(np.loadtxt(os.path.join(C.data_dir, "meshPosition_all.txt"))).to("cuda")
+    position_pivotal = torch.from_numpy(np.loadtxt(os.path.join(C.data_dir, "meshPosition_pivotal.txt"))).to("cuda")
+    
+    encode_output = enc_doc_model.encode(sample, position_mesh, position_pivotal, 1)
+    
+    print(encode_output)
+    
+    decode_output = enc_doc_model.decode(encode_output, position_mesh, position_pivotal, 1)
+    
+    print(decode_output)
+# %%
